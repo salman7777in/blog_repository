02093ff8706bc01800b08879288fc06564738fff
salman7777in <salmanazhar7777@gmail.com>
2022-10-10 23:27:23 +0500

    added user-validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-
+    validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum:3, maximum: 25}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum:25}, format: {with: valid_email_regix}
+    before_save { self.email = email.downcase }
 end
