89cca13632ca254aa885fe1a5d25a227da12ca20
salman7777in <salmanazhar7777@gmail.com>
2022-10-11 16:25:08 +0500

    One-to-Many Association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
+    has_many :articles
     validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum:3, maximum: 25}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum:25}, format: {with: valid_email_regix}
+    validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum:25}, format: {with: VALID_EMAIL_REGEX}
     before_save { self.email = email.downcase }
 end
