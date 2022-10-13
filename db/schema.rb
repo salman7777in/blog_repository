cdacadd3dd97d84fef0d4ff92ce813248354cf27
salman7777in <salmanazhar7777@gmail.com>
2022-10-14 00:21:55 +0500

    Signing Up Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_11_103614) do
+ActiveRecord::Schema.define(version: 2022_10_13_184643) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_10_11_103614) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
