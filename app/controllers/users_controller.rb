beba1aa93c8074fcbba2d107232c35573991f33b
salman7777in <salmanazhar7777@gmail.com>
2022-10-18 20:35:23 +0500

    Add Users to Articles Show Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
     before_action :set_params, only: [:edit, :update, :show]
+    def index
+        @users= User.all
+    end
     def new
         @user= User.new
     end
